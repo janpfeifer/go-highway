c8a453eaf755431c501110e22976b9840ed56f2d
AJ Roetker <ajroetker@antfly.io>
2026-01-11 22:26:18 -0800

    gemini: Ignore hwy/c directory in Go builds
    - Add doc.go with //go:build ignore to exclude hwy/c from go build/vet
    - Prevents 'C source files not allowed' error when CGO is disabled
    - Keeps .c files available for GOAT generator

---
+++ hwy/c/doc.go
@@ -0,0 +1,6 @@
+//go:build ignore
+
+// Package c contains C source files for GOAT transpilation.
+// This package is ignored by the Go build system to prevent "C source files not allowed" errors
+// when CGO is disabled.
+package c
